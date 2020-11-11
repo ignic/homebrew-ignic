90d7489b2e0bb0bd37c904ce0f1ac35603eb941c
Ignacio Serrano <ignic@ignic.com>
2020-11-11 21:11:22 +0100

    circuit.rb fix style

--- Casks/circuit.rb
+++ Casks/circuit.rb
@@ -1,11 +1,11 @@
-cask 'circuit' do
+cask "circuit" do
   version :latest
   sha256 :no_check
 
   # eu.yourcircuit.com was verified as official when first introduced to the cask
-  url 'https://eu.yourcircuit.com/desktop/releases/mac/Circuit.dmg'
-  name 'Unify Circuit'
-  homepage 'https://unify.com/en/solutions/team-collaboration/circuit'
+  url "https://eu.yourcircuit.com/desktop/releases/mac/Circuit.dmg"
+  name "Unify Circuit"
+  homepage "https://unify.com/en/solutions/team-collaboration/circuit"
 
-  app 'Circuit.app'
+  app "Circuit.app"
 end
