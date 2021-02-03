239331dc03b81581b9fb47b1a7a220ede8c15ca3
Ignacio Serrano <ignic@ignic.com>
2021-02-03 12:43:53 +0100

    Add font-calibri cask

---
+++ Casks/font-calibri.rb
@@ -0,0 +1,14 @@
+cask "font-calibri" do
+  version "1.0"
+  sha256 "ae0dd6616c343be967912acad10c30fbbcbc915b895f694194f7604ce737e9ab"
+
+  url "https://github.com/ignic/homebrew-ignic/raw/master/assets/calibri.zip"
+  name "Microsoft Office Calibri Font"
+
+  font "calibri.ttf"
+  font "calibrib.ttf"
+  font "calibrii.ttf"
+  font "calibril.ttf"
+  font "calibrili.ttf"
+  font "calibriz.ttf"
+end
